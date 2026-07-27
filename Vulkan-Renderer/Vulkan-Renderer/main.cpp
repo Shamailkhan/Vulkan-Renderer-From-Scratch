a752be970787133d59038909ccacf35be04ce9cb
Shamail_Aamir <shamailkhan757@gmail.com>
2026-07-28 00:13:05 +0500

    created vulkan instance

--- Vulkan-Renderer/Vulkan-Renderer/main.cpp
+++ Vulkan-Renderer/Vulkan-Renderer/main.cpp
@@ -1,8 +1,41 @@
-#include <vulkan/vulkan.h>
+﻿#include <vulkan/vulkan.h>
 #include <iostream>
 
 int main()
 {
    //vulkan triangle rendering
+
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Vulkan Renderer";
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+	//DEFINING A STRUCTURE THAT DESCRIBE THE APPLICATION TO VULKAN 
+	//VULKAN USE U STYPE TO IDENTIFY STRUCT TYPE 
+	//{} IT INITIALIZE ALL THE FEILD TO ZERO OR NULL 
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	VkInstance instance;
+	VkResult result=vkCreateInstance(&createInfo,nullptr,&instance);
+
+	if (result == VK_SUCCESS)
+	{
+		std::cout << "[SUCCESS] Vulkan Instance created successfully.\n";
+		std::cout << "VkResult = " << result << std::endl;
+		std::cout << "Instance Handle = " << instance << std::endl;
+	}
+	else
+	{
+		std::cout << "[ERROR] Failed to create Vulkan Instance.\n";
+		std::cout << "VkResult = " << result << std::endl;
+		return -1;
+	}
+
+	//Cleanup
+	vkDestroyInstance(instance, nullptr);
+	return 0;
+
 }
 
